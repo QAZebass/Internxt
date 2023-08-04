455c1d04e60a8f3c0c36999c8cd248d2504dda9f
QAZebass <ze.basszender@gmail.com>
2023-08-04 10:39:45 -0300

    Adding gitignore and deleting sample tests

--- cypress/integration/modelo.spec.js
+++ cypress/integration/modelo.spec.js
@@ -1,5 +1,8 @@
 describe('Just a test',()=>{
     it('Sample test for new repo',()=>{
         cy.visit('www.google.com')
+        cy.get('[jsname="gLFyf"]').type('Alice in chains wikipedia')
+        cy.get('body').click()
+
     })
 })
