7b0a74cf9f50e74177d2ec83813b0544f93f8628
QAZebass <ze.basszender@gmail.com>
2023-08-04 10:45:02 -0300

    modified spec added

--- cypress/integration/modelo.spec.js
+++ cypress/integration/modelo.spec.js
@@ -1,8 +1,7 @@
 describe('Just a test',()=>{
     it('Sample test for new repo',()=>{
         cy.visit('www.google.com')
-        cy.get('[jsname="gLFyf"]').type('Alice in chains wikipedia')
-        cy.get('body').click()
+        
 
     })
 })
